b827bc0cb42d9367dfde1a79ba21ab524f01bc5c
Jeppe Klitgaard <hi@jeppe.science>
2026-07-11 18:21:27 +0200

    refactor: move scripts to more general static folder

--- src/ezhpcy/cli/tunnel/install.py
+++ src/ezhpcy/cli/tunnel/install.py
@@ -17,8 +17,8 @@ from ezhpcy.config import config
 from ezhpcy.patch.sdist import sdist_for_current_installation
 
 INSTALL_DIR_NAME = "ezhpcy"
-INSTALL_SCRIPT_RESOURCE = "scripts/install.sh"
-UNINSTALL_SCRIPT_RESOURCE = "scripts/uninstall.sh"
+INSTALL_SCRIPT_RESOURCE = "static/install.sh"
+UNINSTALL_SCRIPT_RESOURCE = "static/uninstall.sh"
 
 
 def install_cmd(

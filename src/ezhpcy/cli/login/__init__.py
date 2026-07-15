0b79fe3ae2ebd00b4017df5f35704464c1221d11
Jeppe Klitgaard <hi@jeppe.science>
2026-07-15 16:49:39 +0200

    feat: remove unused login cli app

--- src/ezhpcy/cli/login/__init__.py
+++
@@ -1,9 +0,0 @@
-import typer
-
-from ezhpcy.cli.utils.alias import AliasGroup
-
-login_app = typer.Typer(
-    cls=AliasGroup,
-    help="Commands to be executed at the login node of the HPC.",
-    no_args_is_help=True,
-)
